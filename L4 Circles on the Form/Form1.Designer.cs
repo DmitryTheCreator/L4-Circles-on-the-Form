c79f3ae2aa8d9e0622fdd8ed091c6ad4d1767501
DmitryTheCreator <dmitriyzamorev@gmail.com>
2020-11-21 17:30:12 +0500

    Version 1
    Написал два класса: Circle и Figure. Первый имеет 4 основных поля: x, y, radius, color; второй является хранилищем объектов Circle

---
+++ L4 Circles on the Form/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace L4_Circles_on_the_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(686, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 548);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Salmon;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 448);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(686, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 548);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(944, 604);
+            this.MinimumSize = new System.Drawing.Size(944, 604);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "L4 Circles on the Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ L4 Circles on the Form/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L4_Circles_on_the_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+        
+
+    public class Figure
+    {
+        private LinkedList<Circle> circles = new LinkedList<Circle>();
+
+        public void add_circle(int x, int y, Color color)
+        {
+            // Переход статуса на обычный и выделенный
+            transite_addition();
+            // -----------
+
+            circles.AddLast(new Circle(x, y, color));
+        }
+        public void add_circle(Circle circle)
+        {
+            // Переход статуса на обычный и выделенный
+            transite_addition();
+            // -----------
+
+            circles.AddLast(circle);
+        }
+        public void del_circle(Circle circle)
+        {
+            circles.Remove(circle);
+
+            // Переход текущего статуса
+            transite_deleting();
+            // -----------
+        }
+        public void del_circle(Color color)
+        {
+            foreach (Circle circle in circles)
+            {
+                if (circle.Color == color)
+                    circles.Remove(circle);
+            }
+            // Переход текущего статуса
+            transite_deleting();
+            // -----------
+        }
+        private void transite_addition()
+        {
+            if (circles.Last.Value.Color == Color.Blue)
+                circles.Last.Value.Color = Color.Red;
+            else circles.Last.Value.Color = Color.Green;
+        }
+        private void transite_deleting()
+        {
+            int rand = new Random().Next(0, circles.Count);
+            int count = 0;
+            foreach (Circle c in circles)
+            {
+                if (rand == count)
+                    c.Color = Color.Blue;
+                count++;
+            }
+        }
+    }
+
+
+
+    public class Circle 
+    {       
+        private int x, y;
+        public int X { set { x = value; } get { return x; } }
+        public int Y { set { y = value; } get { return y; } }
+
+        private const int radius = 25;
+        public int Radius { get { return radius; } }
+
+        private Color color;
+        public Color Color { set { color = value; } get { return color; } }
+
+        public Circle(int x, int y, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+}
